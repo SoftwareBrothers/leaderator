6228cc4187e38551b6933d63b1c62c3c39712448
Paweł Jarosz <pawel.jarosz@softwarebrothers.co>
2019-05-24 14:29:47 +0200

    Implement first endpoint /email/verify with parameter validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { EmailController } from './email/email.controller';
 import { AppService } from './app.service';
+import { EmailService } from './email/email.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, EmailController],
+  providers: [AppService, EmailService],
 })
 export class AppModule {}
