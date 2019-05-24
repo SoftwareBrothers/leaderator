21636e5568706f76237ce803d828d63748eccfd8
marek <marek.firlejczyk@rst-it.com>
2019-05-24 13:17:37 +0200

    Config mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { UserService } from './user/user.service';
-import { ConfigModule } from './config/config.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {ConfigModule} from './config/config.module';
+import {UserModule} from './user/user.module';
+import {AuthModule} from './auth/auth.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ConfigModule,
-    UserModule,
-    AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'datdbabase.db',
-      synchronize: true,
-      logging: false,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    }),
-  ],
-  controllers: [
-    AppController,
-  ],
+    imports: [
+        ConfigModule,
+        UserModule,
+        AuthModule,
+        TypeOrmModule.forRoot(),
+    ],
+    controllers: [
+        AppController,
+    ],
 })
-export class AppModule { }
+export class AppModule {
+}
