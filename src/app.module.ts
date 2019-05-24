ab93b8e80101d2294963777eee6143ea035407ec
marek <marek.firlejczyk@rst-it.com>
2019-05-24 16:37:53 +0200

    Simply docker db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,13 @@ import {ConfigModule} from './config/config.module';
 import {UserModule} from './user/user.module';
 import {AuthModule} from './auth/auth.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
-import {ConfigService} from './config/config.service';
 
 @Module({
     imports: [
         ConfigModule,
         UserModule,
         AuthModule,
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (config: ConfigService) => config.getDB(__dirname),
-            inject: [ConfigService],
-        }),
+        TypeOrmModule.forRoot(),
     ],
     controllers: [AppController],
 })
